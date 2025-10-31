2f1671497968794f8e3995a914d4ee0e645061f6
SebastianFuentesAvalos <sf2022073902@virtual.upt.pe>
2025-10-30 22:32:00 -0500

    fe

---
+++ customer-app/libs/domain/src/index.ts
@@ -0,0 +1,7 @@
+export * from './domain.module';
+export * from './domain.service';
+export * from './customer';
+export * from './customer-registration';
+export * from './data-access-layer';
+export * from './email-service';
+export * from './validator';
